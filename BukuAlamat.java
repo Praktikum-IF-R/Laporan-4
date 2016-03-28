677e77c856456afbee4033582af4feb17c91f0ae
AnitaSumiati <anitasumiati2@gmail.com>
2016-03-28 15:54:14 +0700

    155150201111333_ANITA SUMIATI

--- BukuAlamat.java
+++ BukuAlamat.java
@@ -1,36 +1,38 @@
 package implementasi;
 public class BukuAlamat {
-    private String []nama=new String [100];
-    private String []alamat=new String[100];
-    private String []notel =new String[100];
-    private String []email=new String[100];
-    
-    private static int count;
-    private static int temp;
-    
-    public BukuAlamat(){
-        count=0;
-    }
-    public String getNama(int temp){
-        return nama[temp];
-    }
-    public String getAlamat(int temp){
-        return alamat[temp];
-    }
-    public String getNoTelpon(int temp){
-        return notel[temp];
-    }
-    public String getEmail(int temp){
-        return email[temp];
-    }
-    public void setNama(String Nama ,int temp ,String Alamat ,String Notel ,String Email){
-        nama[temp] = Nama;
-        alamat [temp]=Alamat;
-        notel [temp] =Notel;
-        email [temp] =Email;
-        count++;
-    }
-    public static int getCount(){
-        return count;
+private String nama;
+private String alamat;
+private String telepon;
+private String email;
+private static int Count;
+
+public BukuAlamat(){Count++ ;}
+
+public void setNama( String temp ){
+nama = temp;
+}
+public String getNama(){
+return nama;
+}
+public void setAlamat( String temp ){
+alamat = temp;
+}
+public String getAlamat(){
+return alamat;
+}
+public void setTelepon( String temp ){
+telepon = temp;
+}
+public String getTelepon(){
+return telepon;
+}
+public void setEmail( String temp ){
+email = temp;
+}
+public String getEmail(){ 
+return email;
+}
+public static int getCount(){
+return Count;
     }
 }
--- MainBukuAlamat.java
+++ MainBukuAlamat.java
@@ -1,52 +1,35 @@
 package implementasi;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-
+import java.util.Scanner;
 public class MainBukuAlamat {
     public static void main(String [] args){
-BufferedReader dataIn=new BufferedReader(new InputStreamReader(System.in));
-String [] nama =new String [100];
-String [] alamat =new String [100];
-String [] notel=new String [100];
-String [] email=new String [100];
+BukuAlamat Anita = new BukuAlamat();
+BukuAlamat Olta = new BukuAlamat();
 
-BukuAlamat DataBuku =new BukuAlamat();
-for (int data =0; data<3; data++){
-    System.out.print("Masukan Nama:");
-                try{
-                    nama[data]=dataIn.readLine();
-                }catch(IOException e){
-                    System.out.print("Eror");
-                }
-    System.out.print("Masukan Alamat:");
-                try{
-                    alamat[data]=dataIn.readLine();
-                }catch(IOException e){
-                    System.out.print("Eror");
-                }
-    System.out.print("Masukan No Telepon:");
-                try{
-                    notel[data]=dataIn.readLine();
-                }catch(IOException e){
-                    System.out.print("Eror");
-                }
-    System.out.print("Masukan E-Mail:");
-                try{
-                    email[data]=dataIn.readLine();
-                }catch(IOException e){
-                    System.out.print("Eror");
-                }            
-}
-for(int data =0; data<3; data++){
-    DataBuku.setNama(nama [data],data,alamat[data],notel[data],email[data]);
-    System.out.println("---------------------------------------------");
-    System.out.println("Nama            :"+DataBuku.getNama(data));
-    System.out.println("Alamat          :"+DataBuku.getAlamat(data));
-    System.out.println("No Telepon      :"+DataBuku.getNoTelpon(data));
-    System.out.println("Alamat E-Mail   :"+DataBuku.getEmail(data));
-    System.out.println("---------------------------------------------");
-}
-        System.out.println("\nTotal Jumlah Siswa :"+BukuAlamat.getCount());
-    }
+Anita.setNama("Anita Sumiati Siahaan");
+Anita.setAlamat("JL.R nurdin Sinunukan ");
+Anita.setTelepon("081234567890");
+Anita.setEmail("anitasumiatiS@gmail.com");
+
+Olta.setNama("Olta Patrobas Hutabarat");
+Olta.setAlamat("JL.Raja Inal Siregar");
+Olta.setTelepon("081209876543");
+Olta.setEmail("oltapatrobasH@gmail.com");
+
+System.out.println( "-------------------------------------------" );
+System.out.println( "Nama \t\t:"+Anita.getNama() );
+System.out.println( "Alamat \t\t:"+Anita.getAlamat() );
+System.out.println( "No Telepon \t:"+Anita.getTelepon() );
+System.out.println( "Alamat E-mail \t:"+Anita.getEmail ()+ "\n ");
+System.out.println( "-------------------------------------------" );
+
+System.out.println( "-------------------------------------------" );
+System.out.println( "Nama \t\t:"+Olta.getNama() );
+System.out.println( "Alamat \t\t:"+Olta.getAlamat() );
+System.out.println( "No Telepon \t:"+Olta.getTelepon() );
+System.out.println( "Alamat E-mail\t:"+Olta.getEmail() );
+System.out.println( "-------------------------------------------" );
+
+System.out.println("Jumlah  ="+BukuAlamat.getCount());
+
+} 
 }
