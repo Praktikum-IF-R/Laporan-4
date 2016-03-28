4778a60352879ebfccfe50d7e2b6e4c4414a698b
kadektop2 <kadektop2@gmail.com>
2016-03-28 15:56:13 +0700

    Revert "155150201111328_IMadeNarendraPutra"
    This reverts commit 04ec8e84d2a8866bf808a7f03469d7947eef4896.

--- Bab4/addressbook.java
+++ Bab4/addressbook.java
@@ -2,4 +2,46 @@ package Bab4;
 
 public class addressbook {
 
+    private String nama, alamat, email;
+    private int telp;
+
+    public addressbook(String nama, String alamat, int telp, String email) {
+        nama = "undefined";
+        alamat = "null";
+        telp = 404;
+        email = "null";
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getTelp() {
+        return telp;
+    }
+
+    public void setTelp(int telp) {
+        this.telp = telp;
+    }
+
 }
